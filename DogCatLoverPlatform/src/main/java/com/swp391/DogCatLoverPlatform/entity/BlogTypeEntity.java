aa3ab3ee37107efedaae35a0fd4c9e49dabc96b9
milod172 <ngxdung172@gmail.com>
2023-09-22 12:49:02 +0700

    Đã sửa Khung Project

---
+++ DogCatLoverPlatform/src/main/java/com/swp391/DogCatLoverPlatform/entity/BlogTypeEntity.java
@@ -0,0 +1,17 @@
+package com.swp391.DogCatLoverPlatform.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name= "blog_type")
+public class BlogTypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "blogTypeEntity") //Checked
+    List<BlogEntity> blogEntity;
+}
---
+++ DogCatLoverPlatform/src/main/java/com/swp391/DogCatLoverPlatform/entity/BookingEntity.java
@@ -0,0 +1,40 @@
+package com.swp391.DogCatLoverPlatform.entity;
+
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity(name = "booking")
+public class BookingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "create_date")
+    private Date create_date;
+
+    @Column(name = "total_price")
+    private double total_price;
+
+    @Column(name = "paying_method")
+    private String paying_method;
+
+    @Column(name = "status")
+    private boolean status;
+
+    @ManyToOne
+    @JoinColumn(name="id_user") // tên cột khóa ngoại trong database   //Checked
+    private UserEntity userEntity_BookingEntity; // tên Entity tham chiếu tới
+
+    @ManyToOne
+    @JoinColumn(name="id_blog") // tên cột khóa ngoại trong database   //Checked
+    private BlogEntity blogEntity_BookingEntity; // tên Entity tham chiếu tới
+
+
+    @OneToMany(mappedBy = "bookingEntity")
+    List<BookingHistoryEntity> listBookingHistory;
+
+
+}
